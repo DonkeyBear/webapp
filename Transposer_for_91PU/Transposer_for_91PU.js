afb95a544a47456c9f52bd68a973887b8e423a1c
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2021-10-17 04:11:48 +0800

    Upload Transposer for 91PU

---
+++ Transposer_for_91PU/Transposer_for_91PU.js
@@ -0,0 +1,136 @@
+function transpose() {
+  var keys = {
+    "C": "[I]"  , "C#": "[I#]" ,
+    "D": "[II]" , "D#": "[II#]",
+    "E": "[III]",
+    "F": "[IV]" , "F#": "[IV#]",
+    "G": "[V]"  , "G#": "[V#]" ,
+    "A": "[VI]" , "A#": "[VI#]",
+    "B": "[VII]"
+  };
+  var pitchNameFix = {
+    "#b" : "" , "b#" : "" ,
+    "E#" : "F", "Fb" : "E",
+    "B#" : "C", "Cb" : "B",
+    "C##": "D", "D##": "E",
+    "F##": "G", "G##": "A",
+    "A##": "B"
+  };
+  
+  var chordSheet = document.getElementById("chordSheet").value;
+  var capo = Number(document.getElementById("capo").value);
+  
+  // input valid
+  if ((capo >= -11 && capo <= 11) == false) {
+    alert("請輸入 -11 到 11 之間的值！");
+    return;
+  }
+  
+  var i; // for loop
+  
+  for (i = 0; i < 12; i++) {
+    // first transpose to Roman number
+    chordSheet = chordSheet.replaceAll(
+      Object.keys(keys)[i],                
+      Object.values(keys)[i]
+    );
+  }
+  
+  for (i = 0; i < 12; i++) {
+    // transpose offset
+    var f = i + capo;
+    if (f >= 12) {
+      f -= 12;
+    } else if (f < 0) {
+      f += 12;
+    }
+    // second transpose to pitch names
+    chordSheet = chordSheet.replaceAll(
+      Object.values(keys)[i],     
+      Object.keys(keys)[f]
+    );
+  }
+  
+  for (i = 0; i < 11; i++) {
+    //fix illegal pitch names
+    chordSheet = chordSheet.replaceAll(
+      Object.keys(pitchNameFix)[i], 
+      Object.values(pitchNameFix)[i]
+    );
+  }
+  
+  // fix layout of the chord sheet
+  chordSheet = chordSheet.replaceAll("\n", "<br>");
+  chordSheet = chordSheet.replaceAll(" ", "&nbsp;");
+  
+  // color the chords
+  // (This regex is stole from Alex Golovin in Stack Overflow,
+  // and make some small changes by me.)
+  // ref: https://stackoverflow.com/questions/29144822/
+  chordSheet = chordSheet.replaceAll(
+    /([A-G](#|b)?)(\(?(M|maj|major|m|min|minor|dim|sus|dom|aug)?(\+|-|add)?\d*\)?)-?\d*(\/([A-G](#|b)?))?/g,
+    "<span class=\"chordBlue\">$&</span>"
+  );
+  
+  document.getElementById("output").innerHTML = chordSheet;
+}
+
+function fontSizeDecrease() {
+  var fontSize = document.getElementById("fontSize").innerHTML;
+  fontSize = Number(fontSize) - 1;
+  if (fontSize == 0) {
+    return;
+  }
+  document.getElementById("fontSize").innerHTML = fontSize;
+  document.getElementById("output").style.cssText +=
+    "font-size: " + fontSize + "px;";
+}
+
+function fontSizeIncrease() {
+  var fontSize = document.getElementById("fontSize").innerHTML;
+  fontSize = Number(fontSize) + 1;
+  document.getElementById("fontSize").innerHTML = fontSize;
+  document.getElementById("output").style.cssText +=
+    "font-size: " + fontSize + "px;";
+}
+
+function lineHeightDecrease() {
+  var lineHeight = document.getElementById("lineHeight").innerHTML;
+  if (lineHeight == 0) {
+    return;
+  }
+  lineHeight = Number(lineHeight) - 1;
+  document.getElementById("lineHeight").innerHTML = lineHeight;
+  document.getElementById("output").style.cssText +=
+    "line-height: " + lineHeight + "px;";
+}
+
+function lineHeightIncrease() {
+  var lineHeight = document.getElementById("lineHeight").innerHTML;
+  lineHeight = Number(lineHeight) + 1;
+  document.getElementById("lineHeight").innerHTML = lineHeight;
+  document.getElementById("output").style.cssText +=
+    "line-height: " + lineHeight + "px;";
+}
+
+function paddingDecrease() {
+  var padding = document.getElementById("padding").innerHTML;
+  if (padding < 0) {
+    return;
+  }
+  padding = Number(padding) - 1;
+  document.getElementById("padding").innerHTML = padding;
+  document.getElementById("output").style.cssText +=
+    "padding-left: " + padding + "%;";
+}
+
+function paddingIncrease() {
+  var padding = document.getElementById("padding").innerHTML;
+  if (padding > 100) {
+    return;
+  }
+  padding = Number(padding) + 1;
+  document.getElementById("padding").innerHTML = padding;
+  document.getElementById("output").style.cssText +=
+    "padding-left: " + padding + "%;";
+}
