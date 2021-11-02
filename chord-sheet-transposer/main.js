fc0a5db72a6be05e09d7db44019e5307a0ff56de
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2021-11-02 23:50:53 +0800

    Upgrade with 91PU optimize.

--- chord-sheet-transposer/main.js
+++ chord-sheet-transposer/main.js
@@ -1,21 +1,68 @@
+const keys = {
+  "C": "[I]"  , "C#": "[I#]" ,
+  "D": "[II]" , "D#": "[II#]",
+  "E": "[III]",
+  "F": "[IV]" , "F#": "[IV#]",
+  "G": "[V]"  , "G#": "[V#]" ,
+  "A": "[VI]" , "A#": "[VI#]",
+  "B": "[VII]"
+};
+const pitchNameFix = {
+  "#b" : "" , "b#" : "" ,
+  "E#" : "F", "Fb" : "E",
+  "B#" : "C", "Cb" : "B",
+  "C##": "D", "D##": "E",
+  "F##": "G", "G##": "A",
+  "A##": "B"
+};
+
 function transpose() {
-  var keys = {
-    "C": "[I]"  , "C#": "[I#]" ,
-    "D": "[II]" , "D#": "[II#]",
-    "E": "[III]",
-    "F": "[IV]" , "F#": "[IV#]",
-    "G": "[V]"  , "G#": "[V#]" ,
-    "A": "[VI]" , "A#": "[VI#]",
-    "B": "[VII]"
-  };
-  var pitchNameFix = {
-    "#b" : "" , "b#" : "" ,
-    "E#" : "F", "Fb" : "E",
-    "B#" : "C", "Cb" : "B",
-    "C##": "D", "D##": "E",
-    "F##": "G", "G##": "A",
-    "A##": "B"
-  };
+
+  function transposeThis(targetString) {
+    for (let i = 0; i < 12; i++) {
+      // first transpose to Roman number
+      targetString = targetString.replaceAll(
+        Object.keys(keys)[i],
+        Object.values(keys)[i]
+      );
+    }
+
+    for (let i = 0; i < 12; i++) {
+      // transpose offset
+      let f = i + transposeValue;
+      if (f >= 12) {
+        f -= 12;
+      } else if (f < 0) {
+        f += 12;
+      }
+      // second transpose to pitch names
+      targetString = targetString.replaceAll(
+        Object.values(keys)[i],
+        Object.keys(keys)[f]
+      );
+    }
+
+    for (let i = 0; i < 11; i++) {
+      //fix illegal pitch names
+      targetString = targetString.replaceAll(
+        Object.keys(pitchNameFix)[i],
+        Object.values(pitchNameFix)[i]
+      );
+    }
+
+    // fix layout of the chord sheet
+    targetString = targetString.replaceAll("\n", "<br>");
+    targetString = targetString.replaceAll(" ", "&nbsp;");
+
+    // color the chords
+    // ref for this regex: https://stackoverflow.com/questions/29144822/
+    targetString = targetString.replaceAll(
+      /([A-G](#|b)?)(\(?(M|maj|major|m|min|minor|dim|sus|dom|aug)?(\+|-|add)?\d*\)?)(\+|-)?\d*\(?(M|maj|major)?\d*\)?(\/(([A-G]|\d*)(#|b)?))?/g,
+      "<span class=\"is-chord\">$&</span>"
+    );
+
+    return targetString;
+  }
 
   var chordSheet = document.getElementById("chordSheet").value;
   var transposeType = document.getElementById("select-transpose-type").value;
@@ -36,49 +83,26 @@ function transpose() {
     transposeValue = 0 - transposeValue;
   }
 
-  for (let i = 0; i < 12; i++) {
-    // first transpose to Roman number
-    chordSheet = chordSheet.replaceAll(
-      Object.keys(keys)[i],
-      Object.values(keys)[i]
-    );
-  }
-
-  for (let i = 0; i < 12; i++) {
-    // transpose offset
-    let f = i + transposeValue;
-    if (f >= 12) {
-      f -= 12;
-    } else if (f < 0) {
-      f += 12;
+  if (document.getElementById("check-91pu-optimize").checked) {
+    chordSheetArray = chordSheet.split("\n");
+    for (let i = 0; i < chordSheetArray.length; i++) {
+      if (chordSheetArray[i].indexOf("|") != -1) {
+        chordSheetArray[i] = transposeThis(chordSheetArray[i]);
+      } else {
+        chordSheetArray[i] = chordSheetArray[i].replaceAll(" ", "&nbsp;");
+      }
     }
-    // second transpose to pitch names
-    chordSheet = chordSheet.replaceAll(
-      Object.values(keys)[i],
-      Object.keys(keys)[f]
-    );
+    chordSheet = chordSheetArray.join("<br>");
+  } else {
+    chordSheet = transposeThis(chordSheet);
   }
 
-  for (let i = 0; i < 11; i++) {
-    //fix illegal pitch names
-    chordSheet = chordSheet.replaceAll(
-      Object.keys(pitchNameFix)[i],
-      Object.values(pitchNameFix)[i]
-    );
+  if (document.getElementById("check-replace-sharp-flat").checked) {
+    chordSheet = chordSheet.replaceAll(/[A-G]#/g, "$&SHARP-SYMBOL");
+    chordSheet = chordSheet.replaceAll(/[A-G]b/g, "$&FLAT-SYMBOL");
+    chordSheet = chordSheet.replaceAll("#SHARP-SYMBOL", "<span class=\"music-symbol\">♯</span>");
+    chordSheet = chordSheet.replaceAll("bFLAT-SYMBOL", "<span class=\"music-symbol\">♭</span>");
   }
-
-  // fix layout of the chord sheet
-  chordSheet = chordSheet.replaceAll("\n", "<br>");
-  chordSheet = chordSheet.replaceAll(" ", "&nbsp;");
-
-  // color the chords
-  // (This regex is stole from Alex Golovin in Stack Overflow,
-  // and make some small changes by me.)
-  // ref: https://stackoverflow.com/questions/29144822/
-  chordSheet = chordSheet.replaceAll(
-    /([A-G](#|b)?)(\(?(M|maj|major|m|min|minor|dim|sus|dom|aug)?(\+|-|add)?\d*\)?)(\+|-)?\d*\(?(M|maj|major)?\d*\)?(\/([A-G](#|b)?))?/g,
-    "<span class=\"chordBlue\">$&</span>"
-  );
 
   document.getElementById("output").innerHTML = chordSheet;
 }
