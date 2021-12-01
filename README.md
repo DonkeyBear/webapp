d2e460e628d833d3ed835ef95fe757952fd85122
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2021-12-01 11:49:13 +0800

    Upload "Chordsheet Title Generator"

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Web Apps by DonkeyBear
 * [和弦譜移調器](https://donkeybear.github.io/webapp/chordsheet-transposer/)
+* [和弦譜標題產生器](https://donkeybear.github.io/webapp/chordsheet-title-generator/)
 * [Humble Bundle 鎖區資訊格式器](https://donkeybear.github.io/webapp/hb-region-lock-info-formatter/)
---
+++ chordsheet-title-generator/main.js
@@ -0,0 +1,88 @@
+var colorPalette = {
+  palette_default: ["#E2DCD5", "#4F5161", "#6B2F37", "#515035", "#333333"]
+  //                 white   ,  blue    ,  red     ,  green   ,  deep-gray
+};
+
+clearInput();
+generate();
+
+function get(value) {
+  return document.getElementById(value);
+}
+
+function generate() {
+
+  var selectPalette = get("input-palette").value;
+  var inputSongname = get("input-songname").value.trim();
+  var inputSinger = get("input-singer").value.trim();
+  var inputKey = get("input-key").value.trim();
+  var inputPlay = get("input-play").value.trim();
+  var inputCapo = get("input-capo").value.trim();
+
+  var getOutputContainer = get("output-container");
+  var getOutputTitle = get("output-title");
+  var getOutputDetail = get("output-detail");
+  var getOutputSection = get("output-section");
+
+  var outputString = "";
+
+  getOutputSection.style.cssText +=
+    "background-color: " + colorPalette[selectPalette][0] + ";";
+  getOutputDetail.style.cssText +=
+    "background-color: " + colorPalette[selectPalette][4] + ";";
+
+  if (get("input-voicetype-male").checked) {
+    getOutputTitle.style.cssText +=
+      "color: " + colorPalette[selectPalette][1] + ";";
+  } else if (get("input-voicetype-female").checked) {
+    getOutputTitle.style.cssText +=
+      "color: " + colorPalette[selectPalette][2] + ";";
+  } else if (get("input-voicetype-chorus").checked) {
+    getOutputTitle.style.cssText +=
+      "color: " + colorPalette[selectPalette][3] + ";";
+  } else {
+    getOutputTitle.style.cssText +=
+      "color: " + colorPalette[selectPalette][4] + ";";
+  }
+
+  if (get("input-align-left").checked) {
+    getOutputContainer.style.cssText += "text-align: left;";
+  } else if (get("input-align-center").checked) {
+    getOutputContainer.style.cssText += "text-align: center;";
+  } else {
+    getOutputContainer.style.cssText += "text-align: right;";
+  }
+
+  getOutputTitle.innerHTML = inputSongname;
+
+  if (inputSinger != "") {
+    outputString += "原唱：" + inputSinger + "&emsp;/&emsp;";
+  }
+  if (inputKey != "") {
+    outputString += "Key：" + inputKey + "&emsp;/&emsp;";
+  }
+  if (inputPlay != "") {
+    outputString += "Play：" + inputPlay + "&emsp;/&emsp;";
+  }
+  if (inputCapo != "") {
+    outputString += "Capo：" + inputCapo + "&emsp;/&emsp;";
+  }
+
+  if (getOutputDetail != null) {
+    getOutputDetail.innerHTML = outputString.slice(0, -13);
+  }
+}
+
+function clearInput() {
+  let inputs = [
+    "input-songname",
+    "input-singer",
+    "input-key",
+    "input-play",
+    "input-capo"
+  ];
+  for (let i = 0; i < inputs.length; i++) {
+    get(inputs[i]).value = "";
+  }
+  get("input-songname").focus();
+}
