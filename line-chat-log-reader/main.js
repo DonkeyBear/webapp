45b6a5374dbfa3c7ee20b31f06236e36ddc9528f
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2021-12-13 16:09:36 +0800

    Update to v0.3
    Re-adjust style with CSS.
    Add dark-mode feature.
    Add clean-up feature.
    Add line-count feature.
    Add anchor at date-stamp.

--- line-chat-log-reader/main.js
+++ line-chat-log-reader/main.js
@@ -1,12 +1,14 @@
-let myName;
 let multiLine;
 let multiLineTime;
 let multiLineFrom;
 let multiLineSelf;
+let dateRegExp = /^\d\d\d\d\/\d\d\/\d\d/; // etc. "2021/12/08" would be matched.
 
-updateMyName();
+themeChange();
+lineCount();
 
 function run() {
+  let myName = document.getElementById("my-name").value.trim();
   let chatLogArray = document.getElementById("chat-log").value.split("\n");
 
   for (let i of chatLogArray) {
@@ -49,7 +51,7 @@ function run() {
         let newElement = document.createElement("div");
         multiLineSelf += "<br>" + i.slice(0, -1);
         multiLine = false;
-        
+
         if (multiLineFrom == myName) {
           newElement.setAttribute("class", "chat-bubble chat-self");
           newElement.innerHTML = "<div class='time-stamp time-self'>" + multiLineTime + "</div>" + multiLineSelf;
@@ -66,14 +68,55 @@ function run() {
 
     } else if (i != "") {
       let newElement = document.createElement("div");
+      let matchResult = i.match(dateRegExp);
       newElement.setAttribute("class", "neutral-message date-stamp");
+      if (matchResult != null) {
+        let anchorName = matchResult[0].replaceAll("/", "-");
+        newElement.setAttribute("id", anchorName);
+      }
       newElement.innerHTML = i;
       document.getElementById("output-container").appendChild(newElement);
-    
+
     }
   }
 }
 
-function updateMyName() {
-  myName = document.getElementById("my-name").value.trim();
+function lineCount() {
+  let chatLog = document.getElementById("chat-log").value;
+  document.getElementById("line-count").value = "共 " + chatLog.split("\n").length + " 行";
+}
+
+function themeChange() {
+  if (document.getElementById("themeCss")) {
+    document.getElementById("themeCss").remove();
+  }
+
+  let head = document.head;
+  let link = document.createElement("link");
+  let themeSelect = document.getElementById("themeSelect").value;
+
+  link.id = "themeCss";
+  link.rel = "stylesheet";
+
+  if (themeSelect == "default") {
+    link.href = "default_theme.css";
+  } else if (themeSelect == "dark-mode") {
+    link.href = "dark_mode_theme.css";
+  }
+
+  head.appendChild(link);
+}
+
+function cleanUp() {
+  let outputContainer = document.getElementById("output-container");
+  let outputContainerRow = document.getElementById("output-container-row");
+  let newElement = document.createElement("div");
+
+  outputContainer.remove();
+  newElement.id = "output-container";
+  newElement.className = "col-6 offset-3 min-vh-100";
+  outputContainerRow.appendChild(newElement);
+
+  document.getElementById("chat-log").value = "";
+  document.getElementById("my-name").value = "";
 }
