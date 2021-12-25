5e8bd0ac3031ccd41b02d0b31721ceb8f41f661b
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2021-12-25 17:47:09 +0800

    Update gapminder-test.

--- gapminder-test/main.js
+++ gapminder-test/main.js
@@ -3,11 +3,10 @@ let radios = document.getElementsByClassName("form-check-input");
 for (let i of radios) {
   i.checked = false;
 }
-// rounded-pill bg-success
-// 1.C  2.B  3.C  4.C  5.C  6.B  7.C  8.A  9.C  10.A  11.C  12.C  13.A
+
 function main() {
   let correctAns = document.getElementsByClassName("correct-ans");
-  let correctAnsDiv = document.getElementsByClassName("div-correct-ans");
+  let wrongAns = document.querySelectorAll(".form-check-input:not(.correct-ans):checked");
   let correctAnsCount = 0;
 
   // count the correct answer and print it.
@@ -17,8 +16,11 @@ function main() {
     }
   }
   document.getElementById("correct-ans-count").innerHTML = correctAnsCount;
-  // change color into green.
-  for (let i of correctAnsDiv) {
-    i.className += " rounded-pill bg-success text-light";
+  // change background-color correct and wrong answer into green and red.
+  for (let i of correctAns) {
+    i.parentElement.className += " rounded-pill bg-success text-light";
+  }
+  for (let i of wrongAns) {
+    i.parentElement.className += " rounded-pill bg-danger text-light";
   }
 }
