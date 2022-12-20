5b2d2cfbd3ef93b6eae85b6e831952d8f5a80ae8
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-12-21 01:33:54 +0800

    Update to v2.0 with Vue

--- chordsheet-title-generator/main.js
+++ chordsheet-title-generator/main.js
@@ -1,88 +1,54 @@
-var colorPalette = {
-  palette_default: ["#E2DCD5", "#4F5161", "#6B2F37", "#515035", "#333333"]
-  //                 white   ,  blue    ,  red     ,  green   ,  deep-gray
-};
-
-clearInput();
-generate();
-
-function get(value) {
-  return document.getElementById(value);
-}
-
-function generate() {
-
-  var selectPalette = get("input-palette").value;
-  var inputSongname = get("input-songname").value.trim();
-  var inputSinger = get("input-singer").value.trim();
-  var inputKey = get("input-key").value.trim();
-  var inputPlay = get("input-play").value.trim();
-  var inputCapo = get("input-capo").value.trim();
-
-  var getOutputContainer = get("output-container");
-  var getOutputTitle = get("output-title");
-  var getOutputDetail = get("output-detail");
-  var getOutputSection = get("output-section");
-
-  var outputString = "";
-
-  getOutputSection.style.cssText +=
-    "background-color: " + colorPalette[selectPalette][0] + ";";
-  getOutputDetail.style.cssText +=
-    "color: " + colorPalette[selectPalette][4] + ";";
-
-  if (get("input-voicetype-male").checked) {
-    getOutputTitle.style.cssText +=
-      "color: " + colorPalette[selectPalette][1] + ";";
-  } else if (get("input-voicetype-female").checked) {
-    getOutputTitle.style.cssText +=
-      "color: " + colorPalette[selectPalette][2] + ";";
-  } else if (get("input-voicetype-chorus").checked) {
-    getOutputTitle.style.cssText +=
-      "color: " + colorPalette[selectPalette][3] + ";";
-  } else {
-    getOutputTitle.style.cssText +=
-      "color: " + colorPalette[selectPalette][4] + ";";
+const app = Vue.createApp({
+  data() {
+    return {
+      song: {
+        name: "",
+        singer: "",
+        part: "part-none",
+        key: "",
+        play: "",
+        capo: "",
+        align: "text-start",
+        theme: "theme-default",
+        meta: ""
+      },
+      options: {
+        parts: [
+          { label: "無", value: "part-none", id: "options-parts-none" },
+          { label: "男", value: "part-male", id: "options-parts-male" },
+          { label: "女", value: "part-female", id: "options-parts-female" },
+          { label: "合", value: "part-both", id: "options-parts-both" }
+        ],
+        aligns: [
+          { label: "左", value: "text-start", id: "options-aligns-left" },
+          { label: "中", value: "text-center", id: "options-aligns-center" },
+          { label: "右", value: "text-end", id: "options-aligns-right" }
+        ],
+        themes: [
+          { label: "預設", value: "theme-default" },
+          { label: "深色", value: "theme-dark" }
+        ]
+      }
+    }
+  },
+  methods: {
+    generateMeta() {
+      let metaString = "";
+      if (this.song.singer.trim()) {
+        metaString += `Singer：${this.song.singer}　/　`;
+      }
+      if (this.song.key.trim()) {
+        metaString += `Key：${this.song.key}　/　`;
+      }
+      if (this.song.play.trim()) {
+        metaString += `Play：${this.song.play}　/　`;
+      }
+      if (this.song.capo.trim()) {
+        metaString += `Capo：${this.song.capo}　/　`;
+      }
+      this.song.meta = metaString.slice(0, -3);
+    }
   }
+});
 
-  if (get("input-align-left").checked) {
-    getOutputContainer.style.cssText += "text-align: left;";
-  } else if (get("input-align-center").checked) {
-    getOutputContainer.style.cssText += "text-align: center;";
-  } else {
-    getOutputContainer.style.cssText += "text-align: right;";
-  }
-
-  getOutputTitle.innerHTML = inputSongname;
-
-  if (inputSinger != "") {
-    outputString += "原唱：" + inputSinger + "&emsp;/&emsp;";
-  }
-  if (inputKey != "") {
-    outputString += "Key：" + inputKey + "&emsp;/&emsp;";
-  }
-  if (inputPlay != "") {
-    outputString += "Play：" + inputPlay + "&emsp;/&emsp;";
-  }
-  if (inputCapo != "") {
-    outputString += "Capo：" + inputCapo + "&emsp;/&emsp;";
-  }
-
-  if (getOutputDetail != null) {
-    getOutputDetail.innerHTML = outputString.slice(0, -13);
-  }
-}
-
-function clearInput() {
-  let inputs = [
-    "input-songname",
-    "input-singer",
-    "input-key",
-    "input-play",
-    "input-capo"
-  ];
-  for (let i = 0; i < inputs.length; i++) {
-    get(inputs[i]).value = "";
-  }
-  get("input-songname").focus();
-}
+app.mount("#app-container");
