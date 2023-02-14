5d9fb6ef1eac7408f2d1e26c53b4788360a6c43a
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-02-14 08:51:59 +0800

    Push 前段時間擱置的改動

--- 91-song-book/main.js
+++ 91-song-book/main.js
@@ -26,28 +26,22 @@ const app = Vue.createApp({
     clearInput() {
       this.searchInput = "";
       this.searchByName();
+    },
+    shuffleArray(array) {
+      // ref: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+      let currentIndex = array.length, randomIndex;
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+      }
+      return array;
     }
   }
 });
-
-/* const songBlock = Vue.createApp({
-  data() {
-    return {
-    };
-  },
-  template: `
-    <div v-for="song in songs" class="col">
-      <div class="card h-100">
-        <div class="card-body">
-          <a href="#" class="stretched-link"></a>
-          <h5 class="card-title mb-0">{{ song.name }}</h5>
-          <small class="card-subtitle mb-2 text-muted">{{ song.singer }}</small>
-          <p class="card-text">內文</p>
-        </div>
-      </div>
-    </div>
-  `
-}); */
 
 app.mount("#app");
 
