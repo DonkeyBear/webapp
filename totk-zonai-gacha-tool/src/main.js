60ca1a33755d099636be778917460a0955efd491
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-06-06 03:26:41 +0800

    加入 vue-gtag 追蹤

--- totk-zonai-gacha-tool/src/main.js
+++ totk-zonai-gacha-tool/src/main.js
@@ -5,6 +5,7 @@ import '@picocss/pico';
 import '@picocss/pico/docs/js/modal.js';
 import { createRouter, createWebHistory } from 'vue-router';
 import { createPinia } from 'pinia';
+import VueGtagPlugin from 'vue-gtag';
 
 import HomePage from './components/HomePage.vue';
 import AboutPage from './components/AboutPage.vue';
@@ -24,4 +25,7 @@ const app = createApp(App);
 
 app.use(router);
 app.use(pinia);
+app.use(VueGtagPlugin, {
+  config: { id: 'G-BJVSCP8FRC' }
+});
 app.mount('#app');
