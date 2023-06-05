ae245eeb4559895410994d9ae7cf0d17fe3ef2a9
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-06-05 23:31:51 +0800

    把版本號移至環境變數

--- totk-zonai-gacha-tool/src/components/AboutPage.vue
+++ totk-zonai-gacha-tool/src/components/AboutPage.vue
@@ -1,10 +1,14 @@
+<script setup>
+const appVersion = import.meta.env.VITE_APP_VERSION;
+</script>
+
 <template>
   <h2>關於本程式</h2>
   <p>這是個用來依照所需的左納烏裝置快速查找左納烏轉蛋機的程式，原先只是為了自用而寫的，但是花了這麼多時間，自己獨享太浪費了，於是放在網路上供所有玩家使用。</p>
   <p>資料來自 PTT 使用者 dbfox（來源<a href="https://www.ptt.cc/bbs/NSwitch/M.1685295609.A.269.html">見此文</a>）。</p>
   <p>如果這個程式對你有幫助，可以考慮到我的 GitHub Repo 給我一個星星喔🌟！</p>
 
-  <small>v1.0.0 (230531)</small>
+  <small>{{ appVersion }}</small>
 </template>
 
 <style scoped>
