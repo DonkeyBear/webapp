e996e68bd7009c7b8300a0c3c6b940263298590b
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-07-04 01:56:42 +0800

    更改路由方式為 createWebHashHistory 後重新佈署測試

--- totk-zonai-gacha-tool/src/main.js
+++ totk-zonai-gacha-tool/src/main.js
@@ -3,17 +3,18 @@ import './style.css';
 import App from './App.vue';
 import '@picocss/pico';
 import '@picocss/pico/docs/js/modal.js';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { createPinia } from 'pinia';
 import VueGtagPlugin from 'vue-gtag';
 
+// Vue 頁面元件
 import HomePage from './components/HomePage.vue';
 import AboutPage from './components/AboutPage.vue';
 
 const distDir = import.meta.env.VITE_DIST_DIR;
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: `${distDir}/`, component: HomePage },
     { path: `${distDir}/about`, component: AboutPage }
